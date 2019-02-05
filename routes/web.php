b86a22ea99d93f7f707419bd9aad71ecf5333d32
joseaki <joseaki@icloud.com>
2019-02-05 10:47:24 -0500

    primer commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('mongo', function(Request $request) {
+    $collection = Mongo::get()->controles->test;
+    return $collection->find()->toArray();
+});
